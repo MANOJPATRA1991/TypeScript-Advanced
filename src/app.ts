fa59abb02e0e308e6e191e1e3370869d131ce09c
Manoj Kumar Patra <patra.manoj0@gmail.com>
2018-11-13 20:45:53 +0530

    Add demo for 'this' in different context

--- src/app.ts
+++ src/app.ts
@@ -30,3 +30,32 @@
 // const myInstance = new MyClass();
 // myInstance.myMethod(); // this will refer to MyClass
 
+// ---------------------------------------------------------------------------------------- //
+
+/**
+ * Exploring “this” with .call, .apply and .bind
+ */
+
+// // Object literal
+// const myObj = {
+//     myMethod() {
+//         console.log('Object:::', this);
+//     }
+// };
+
+// // myObj.myMethod();
+
+// // Function
+// function myFunction(...text: string[]) {
+//     console.log('Function:::', this, text);
+// }
+
+// // const bindFunction = myFunction.bind(myObj, 'ABC', 'DEF');
+// const bindFunction = myFunction.bind(myObj); // binds the function to a new context and returns a brand new function with a different context
+// bindFunction('ABC', 'DEF');
+// bindFunction('123', '456');
+// bindFunction('ABC', 'DEF');
+
+// // Difference between call and apply is the way the arguments are passed
+// myFunction.call(myObj, 'ABC', 'DEF'); // Changing the this keyword or the context
+// myFunction.apply(myObj, ['ABC', 'DEF']);
