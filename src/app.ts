0124b6b57ff07ba9a19fc160b159ce454df4dc80
Manoj Kumar Patra <patra.manoj0@gmail.com>
2018-11-14 12:28:25 +0530

    Add demo for intersection types

--- src/app.ts
+++ src/app.ts
@@ -489,3 +489,54 @@
 //     // Playlist
 //     return item.name;
 // }
+
+// ---------------------------------------------------------------------------------------- //
+
+/**
+ * ADVANCED TYPES AND PRACTICES
+ */
+
+// ---------------------------------------------------------------------------------------- //
+
+/**
+ * INTERSECTION TYPES
+ */
+
+// interface Order {
+//     id: string;
+//     amount: number;
+//     currency: string;
+// }
+
+// interface Stripe extends Order {
+//     card: string;
+//     cvc: string;
+// }
+
+// interface PayPal {
+//     email: string;
+// }
+
+// // Intersection Type
+// type CheckoutCard = Order & Stripe;
+// type CheckoutPayPal = Order & PayPal;
+// // type CheckoutABC = Order & { name: string };
+
+// const order: Order = {
+//     id: 'xj28s',
+//     amount: 100,
+//     currency: 'USD'
+// };
+
+// const orderCard: CheckoutCard = {
+//     ...order,
+//     card: '1000 2000 3000 4000',
+//     cvc: '123'
+// };
+
+// const orderPayPal: CheckoutPayPal = {
+//     ...order,
+//     email: 'abc@def.com'
+// };
+
+// const assigned = Object.assign({}, order, orderCard);
